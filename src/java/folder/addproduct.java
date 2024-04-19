98125f3f6b5c0d84539b9ada472594a18695f878
Sampath R P R <127502999+RavinduSampath@users.noreply.github.com>
2024-04-19 20:08:19 +0530

    19

--- src/java/folder/addproduct.java
+++ src/java/folder/addproduct.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package folder;
 
 import java.sql.Connection;
@@ -13,29 +8,24 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author MSI
- */
-class addproduct {
+public class addproduct {
 
-    static List<addproduct> getAllBooks() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private Statement st;
+
+    public void insertproduct(String ProductName, int Price, int ProductQuantity, String ProductDescription, String ProductImage) {
+        connectToDB();
+        String query = "INSERT INTO product VALUES('" + ProductName + "'," + Price + "," + ProductQuantity + ",'" + ProductDescription + "','" + ProductImage + "')";
+        try {
+            st.executeUpdate(query);
+            System.out.println("Record inserted");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
-     Statement st;
-    void insertproduct(String ProductName, int Price, int ProductQuantity, String ProductDescription, String ProductImage) {
-            connectToDB();
-            String query ="INSERT INTO product VALUES('"+ProductName+"',"+Price+","+ProductQuantity+",'"+ProductDescription+"','"+ProductImage+"')";    
-            try {
-                st.executeUpdate(query);
-                System.out.println("Record inserted");
-            } catch (SQLException ex) {
-                System.out.println(ex.getMessage());           
- }
-}
-private void connectToDB() {
-        String driver= "com.mysql.jdbc.Driver";
-        String url="jdbc:mysql://localhost:3306/ecommerce";
+
+    private void connectToDB() {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/ecommerce";
         try {
             Class.forName(driver);
             Connection con = DriverManager.getConnection(url, "root", "");
@@ -45,10 +35,11 @@ private void connectToDB() {
         }
     }
 
+    public static List<addproduct> getAllBooks() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
     void insertproduct(String ProductName, String ProductDescription, String ProductImage, int Price, int ProductQuantity) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-
 }
-   
