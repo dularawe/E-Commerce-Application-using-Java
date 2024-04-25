a445f60829e447fb36e7b2d75d2d68c6e5fb6b05
Sandun Dinesh <sandundinesh21@gmail.com>
2024-04-25 12:39:11 +0530

    5

---
+++ build/web/js/Script.js
@@ -0,0 +1,10 @@
+function clearFields() {
+  document.querySelector(".nameInput").value = "";
+  document.querySelector(".nameInput1").value = "";
+  document.querySelector(".dateInput").value = "";
+  document.querySelector(".addressInput").value = "";
+  document.getElementById("male").checked = false;
+  document.getElementById("female").checked = false;
+  document.querySelector(".numInput").value = "";
+  document.querySelector(".emailInput").value = "";
+}
