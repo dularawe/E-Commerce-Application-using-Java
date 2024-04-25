0d201f255818b11046c48edc99ee011d11fc74e2
Sampath R P R <127502999+RavinduSampath@users.noreply.github.com>
2024-04-25 22:38:45 +0530

    last

---
+++ src/java/folder/addProduct.java
@@ -0,0 +1,46 @@
+package folder;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class addProduct {
+
+    private Statement st;
+
+    public void insertProduct(String ProductName, int Price, int ProductQuantity, String ProductDescription, String ProductImage) {
+        connectToDB();
+        String query = "INSERT INTO products (ProductName, Price, ProductQuantity, ProductDescription, ProductImage) VALUES ('" + ProductName + "', " + Price + ", " + ProductQuantity + ", '" + ProductDescription + "', '" + ProductImage + "')";
+
+        try {
+            st.executeUpdate(query);
+            System.out.println("Record inserted");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private void connectToDB() {
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/ecommerce";
+        try {
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(url, "root", "");
+            st = con.createStatement();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(addProduct.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static List<addProduct> getAllBooks() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    
+}
--- src/java/folder/product.java
+++ src/java/folder/product.java
@@ -1,17 +1,70 @@
-package folder;
 
+import folder.addProduct;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
-
+@WebServlet(name = "product", urlPatterns = {"/product"})
 public class product extends HttpServlet {
 
-    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet product</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet product at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
+        
+        
+        
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
    @Override
 protected void doPost(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
@@ -25,7 +78,19 @@ protected void doPost(HttpServletRequest request, HttpServletResponse response)
     PrintWriter out = response.getWriter();
     out.println("Added Product....");
     
-    addproduct b = new addproduct();
-    b.insertproduct(ProductName,Price,ProductQuantity,ProductDescription,ProductImage);
+    addProduct b = new addProduct();
+    b.insertProduct(ProductName,Price,ProductQuantity,ProductDescription,ProductImage);
 }
+
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
