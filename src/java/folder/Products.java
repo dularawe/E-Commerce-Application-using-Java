d10a787614917e997b7c1c7c088b913a7b00f934
AJPavithra <136953022+AJPavithra@users.noreply.github.com>
2024-04-28 13:37:51 +0530

    changed

--- src/java/folder/Products.java
+++ src/java/folder/Products.java
@@ -1,23 +1,18 @@
 package folder;
 
-import static com.mysql.cj.x.protobuf.MysqlxPrepare.Prepare.OneOfMessage.Type.INSERT;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-/**
- *
- * @author LENOVO
- */
+
 public class Products {
     private Statement st;
     
-    public void  insertProducts(String ProcuctsName, int Price ProductsQuantity, String ProductsDescription, String ProductsImage){
+    public void insertProducts(String ProductsName, int Price, int ProductsQuantity, String ProductsDescription, String ProductsImage){
         connectToDB();
-        String query = INSERT INTO products (ProductsName, Price,ProductsQuantity,ProductsDescription, ProductsImage) VALUES ('" + ProductsName + "', " + Price + ", " + ProductsQuantity + ", '" + ProductsDescription + "', '" + ProductsImage + "')"; 
+        String query = "INSERT INTO products (ProductsName, Price, ProductsQuantity, ProductsDescription, ProductsImage) VALUES ('" + ProductsName + "', '" + Price + "', '" + ProductsQuantity + "', '" + ProductsDescription + "', '" + ProductsImage + "')"; 
      
         try{
             st.executeUpdate(query);
@@ -37,12 +32,10 @@ public class Products {
             st = con.createStatement();
             
         } catch (ClassNotFoundException | SQLException ex) {
-            Logger.getLogger(addProduct.class.getName()).log(Level.SEVERE, null, ex)
+            Logger.getLogger(Products.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
-    public static List<addProduct> getAllBooks(){
-        throw new UnsupportedOperationException("Not supported yet.")
-    }
     
-}
+
+            }
